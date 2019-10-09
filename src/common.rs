f5be10bee47ad38573fa576bf3aa2b598328bfd5
Ales Katona <almindor@gmail.com>
2019-10-09 10:01:05 -0600

    refactor scene into own mod

--- src/common.rs
+++ src/common.rs
@@ -1,12 +1,12 @@
-use crate::components::{Position, Selection, Sprite};
 use serde::{Deserialize, Serialize};
-use specs::{Entities, Join, ReadStorage, WriteStorage};
 use std::env::current_dir;
 use std::path::{Path, PathBuf};
 
 mod action;
+mod scene;
 
 pub use action::Action;
+pub use scene::Scene;
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
 pub struct Texel {
@@ -52,39 +52,6 @@ impl std::fmt::Display for Error {
 impl Error {
     pub fn execution(src: &'static str) -> Self {
         Error::ExecutionError(String::from(src))
-    }
-}
-
-// TODO: figure out a 0-copy way to keep scene serializable/deserializable
-#[derive(Debug, Default, Serialize, Deserialize, Clone)]
-pub struct Scene {
-    pub objects: Vec<(Sprite, Position, bool)>,
-}
-
-impl<'a>
-    From<(
-        &Entities<'a>,
-        &WriteStorage<'a, Sprite>,
-        &WriteStorage<'a, Position>,
-        &ReadStorage<'a, Selection>,
-    )> for Scene
-{
-    fn from(
-        storage: (
-            &Entities,
-            &WriteStorage<'a, Sprite>,
-            &WriteStorage<'a, Position>,
-            &ReadStorage<'a, Selection>,
-        ),
-    ) -> Self {
-        let mut objects = Vec::new();
-        let (e, sp, p, s) = storage;
-
-        for (entity, sprite, pos) in (e, sp, p).join() {
-            objects.push((sprite.clone(), pos.clone(), s.contains(entity)));
-        }
-
-        Scene { objects }
     }
 }
 
---
+++ src/common/scene.rs
@@ -0,0 +1,36 @@
+use serde::{Serialize, Deserialize};
+use specs::{Entities, Join, ReadStorage, WriteStorage};
+use crate::components::{Position, Selection, Sprite};
+
+// TODO: figure out a 0-copy way to keep scene serializable/deserializable
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
+pub struct Scene {
+    pub objects: Vec<(Sprite, Position, bool)>,
+}
+
+impl<'a>
+    From<(
+        &Entities<'a>,
+        &WriteStorage<'a, Sprite>,
+        &WriteStorage<'a, Position>,
+        &ReadStorage<'a, Selection>,
+    )> for Scene
+{
+    fn from(
+        storage: (
+            &Entities,
+            &WriteStorage<'a, Sprite>,
+            &WriteStorage<'a, Position>,
+            &ReadStorage<'a, Selection>,
+        ),
+    ) -> Self {
+        let mut objects = Vec::new();
+        let (e, sp, p, s) = storage;
+
+        for (entity, sprite, pos) in (e, sp, p).join() {
+            objects.push((sprite.clone(), pos.clone(), s.contains(entity)));
+        }
+
+        Scene { objects }
+    }
+}
