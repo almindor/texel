f836ad5d0932a852b7f80af8e28d71aed9cc3db4
Ales Katona <almindor@gmail.com>
2020-01-20 11:19:11 -0700

    WIP for system_data refactor

--- src/systems/action_handler/system_data.rs
+++ src/systems/action_handler/system_data.rs
@@ -10,18 +10,68 @@ pub type SystemData<'a> = (
     ReadStorage<'a, Selection>,
     ReadStorage<'a, Bookmark>,
     WriteStorage<'a, Subselection>,
-    WriteStorage<'a, Position2D>, // cursor position saved to sprite, bookmark position
+    WriteStorage<'a, Position2D>, // cursor position, subselection etc.
     WriteStorage<'a, Dimension>,
     WriteStorage<'a, Sprite>,
     Read<'a, LazyUpdate>,
 );
 
-pub trait SystemDataExt {
+pub trait SystemDataExt<'a> {
     fn state(&mut self) -> &mut State;
+    fn entities(&self) -> &Entities<'a>;
+    fn positions(&mut self) -> &mut WriteStorage<'a, Position>;
+    fn selectable(&self) -> &ReadStorage<'a, Selectable>;
+    fn selected(&self) -> &ReadStorage<'a, Selection>;
+    fn bookmarks(&self) -> &ReadStorage<'a, Bookmark>;
+    fn subselections(&mut self) -> &mut WriteStorage<'a, Subselection>;
+    fn positions_2d(&mut self) -> &mut WriteStorage<'a, Position2D>;
+    fn dimensions(&mut self) -> &mut WriteStorage<'a, Dimension>;
+    fn sprites(&mut self) -> &mut WriteStorage<'a, Sprite>;
+    fn lazy_update(&self) -> &LazyUpdate;
 }
 
-impl SystemDataExt for &mut SystemData<'_> {
+impl<'a> SystemDataExt<'a> for SystemData<'a> {
     fn state(&mut self) -> &mut State {
         &mut self.0
+    }
+
+    fn entities(&self) -> &Entities<'a> {
+        &self.1
+    }
+
+    fn positions(&mut self) -> &mut WriteStorage<'a, Position> {
+        &mut self.2
+    }
+
+    fn selectable(&self) -> &ReadStorage<'a, Selectable> {
+        &self.3
+    }
+
+    fn selected(&self) -> &ReadStorage<'a, Selection> {
+        &self.4
+    }
+
+    fn bookmarks(&self) -> &ReadStorage<'a, Bookmark> {
+        &self.5
+    }
+
+    fn subselections(&mut self) -> &mut WriteStorage<'a, Subselection> {
+        &mut self.6
+    }
+
+    fn positions_2d(&mut self) -> &mut WriteStorage<'a, Position2D> {
+        &mut self.7
+    }
+
+    fn dimensions(&mut self) -> &mut WriteStorage<'a, Dimension> {
+        &mut self.8
+    }
+
+    fn sprites(&mut self) -> &mut WriteStorage<'a, Sprite> {
+        &mut self.9
+    }
+
+    fn lazy_update(&self) -> &LazyUpdate {
+        &self.10
     }
 }
